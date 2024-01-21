06d7f7ee512ad1ec7417cd6a2da929122f287d0d
rsp83 <110043578+rsp83@users.noreply.github.com>
2024-01-21 03:45:18 +0000

    commit

---
+++ unit2_lesson1_Recycling_sort.py
@@ -0,0 +1,9 @@
+material = input("What material is it? ")
+length = float(input("What is its length in cm? "))
+
+if material == "plastic" :
+  waste_type = "recycling"
+if material != "plastic" :
+  waste_type = "trash"
+
+print("Please deposit your item in the " + waste_type + " bin.")
