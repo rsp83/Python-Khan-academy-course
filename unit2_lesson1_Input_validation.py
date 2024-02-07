4e583981e74ebb655067f476a5793555664a9999
rsp83 <110043578+rsp83@users.noreply.github.com>
2024-02-07 00:45:48 +0000

    commit

---
+++ unit2_lesson1_Input_validation.py
@@ -0,0 +1,7 @@
+day = int(input("Enter a day (1-31): "))
+if day < 1 or day > 31:
+    print("Error. Day must be between 1 and 31.")
+
+month = int(input("Enter a month (1-12): "))
+if month < 1 or month > 12:
+    print("Error. Month must be between 1 and 12.")
