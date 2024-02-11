f7165784b79ded97172b8fa0b7c3a0c386bdba4f
rsp83 <110043578+rsp83@users.noreply.github.com>
2024-02-11 22:47:39 +0000

    commit

---
+++ unit2_lesson3_Chained_conditionals.py
@@ -0,0 +1,7 @@
+answer = input("What data type is the value 6.3? ")
+if answer == "float":
+    print("You got it!")
+
+answer = int(input("What does len(\"hi\") evaluate to? "))
+if answer == 2:
+    print("You got it!")
