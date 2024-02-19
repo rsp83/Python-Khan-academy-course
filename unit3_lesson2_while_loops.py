57008c9d3eb2ab6aa2721d5ab74ce0f1022fd702
rsp83 <110043578+rsp83@users.noreply.github.com>
2024-02-19 03:45:19 +0000

    commit

--- unit3_lesson2_while_loops.py
+++ unit3_lesson2_while_loops.py
@@ -1,5 +1,7 @@
 import random
 
+running_total = 0
+
 i = 1
 while i <= 10 :
    first_die = random.randint(1, 6)
@@ -7,4 +9,8 @@ while i <= 10 :
 
    dice_sum = first_die + second_die
    print("You rolled a " + str(dice_sum) + "!")
+   running_total += dice_sum
    i += 1
+
+average = running_total / 10 
+print ("Average of " + str(average))
