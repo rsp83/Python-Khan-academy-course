0a2c3ccce118e412bd357ba833dd44f800f07a93
rsp83 <110043578+rsp83@users.noreply.github.com>
2024-03-04 02:05:50 +0000

    commit

--- unit4_lesson1_function_definitions..py
+++ unit4_lesson1_function_definitions..py
@@ -1,10 +1,14 @@
+def get_time_trophy(hours_played):
+
+    if hours_played >= 50:
+        trophy_name = "Valiant Veteran"
+    elif hours_played >= 10:
+        trophy_name = "Active Adventurer"
+    else:
+        trophy_name = "Trainee Traveller"
+
+    return trophy_name
+
 hours_played = 15
-
-if hours_played >= 50:
-    trophy_name = "Valiant Veteran"
-elif hours_played >= 10:
-    trophy_name = "Active Adventurer"
-else:
-    trophy_name = "Trainee Traveller"
-
-print(trophy_name)
+trophy_Name = get_time_trophy(hours_played)
+print(trophy_Name)
