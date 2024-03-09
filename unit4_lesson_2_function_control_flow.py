720d4fb3f3a725bc975122f77f11e1cc68477552
rsp83 <110043578+rsp83@users.noreply.github.com>
2024-03-09 23:44:14 +0000

    commit

--- unit4_lesson_2_function_control_flow.py
+++ unit4_lesson_2_function_control_flow.py
@@ -11,6 +11,14 @@ def get_banana_score(num_bananas):
     else:
         return 0
 
+
+def get_cherry_score(num_cherries):
+
+    if num_cherries > 1 :
+        return num_cherries // 2 * 5
+    else:
+        return 0 
+
 def get_apple_score(num_apples, has_poison_apple):
     """Returns a player's score based on the number of apple cards.
     The poison apple card turns the apple score negative.
@@ -20,15 +28,8 @@ def get_apple_score(num_apples, has_poison_apple):
     else:
         return num_apples * 2
     
-def get_cherry_score(num_cherries):
 
-    if num_cherries > 1 :
-     return num_cherries // 2 * 5
-    else:
-       return 0 
-    
-
-def get_score(bananas,cherries, apples, has_poison_apple):
+def get_score(bananas, cherries, apples, has_poison_apple):
     """Returns a player's total score based on their cards of each type."""
     banana_score = get_banana_score(bananas)
     cherry_score = get_cherry_score(cherries)
@@ -37,6 +38,6 @@ def get_score(bananas,cherries, apples, has_poison_apple):
     return banana_score + cherry_score + apple_score
 
 # Calculate the final score for each player.
-player1_score = get_score(3,10, 2, True)
-player2_score = get_score(1,47, 5, False)
+player1_score = get_score(3, 3, 2, True)
+player2_score = get_score(1, 4, 5, False)
 print("Scores: p1=" + str(player1_score) + ", p2=" + str(player2_score))
